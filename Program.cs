28079275345fb9b8bbaa8f911b6e1304b994e728
Andressa Dal Col <104804693+Andressadalcol@users.noreply.github.com>
2023-12-24 10:09:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+namespace ValorRaio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double R, A, pi = 3.14159;
+
+
+            R = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+         
+            A = pi* R * R;
+
+            Console.WriteLine(" A =" + A.ToString("f4", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
